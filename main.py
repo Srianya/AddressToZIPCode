28dedda0fc8cca30bbf50c35856046827f6ce4c8
Aastha Saxena <saxena.aastha91@gmail.com>
2022-06-05 00:21:02 -0400

    modified api key permissions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
 import config
+from tkinter import *
 
 # get latitude and longitude from an address
 def get_ltd_lng():
@@ -37,4 +38,25 @@ if __name__ == '__main__':
     latlng = get_ltd_lng()
     get_zipcode(latlng)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Address to zipcode")
+window.config(padx=50, pady=50)
+
+# Add a canvas
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+address_text = canvas.create_text(150, 207, text="Address", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+# Address Entry
+address_entry = Entry(width=35)
+address_entry.grid(row=1, column=0)
+address_entry.focus()
+
+zipcode_button = Button(text="Get me the zip code", highlightthickness=0)
+zipcode_button.grid(row=2, column=0)
+window.mainloop()
